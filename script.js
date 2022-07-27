88f6dcc08d2d63dc3084c9b2a2d3566da5da9401
Aadil-7 <aadilbhusnur007@gmail.com>
2022-07-27 20:33:47 +0530

    100% completed add all the remaining elements.

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 let increament = document.getElementById('person-increament');
 let decreament = document.getElementById('person-decreament');
 let numberOfPers = document.getElementById('numberofperson');
+let billAmount = document.getElementById('billAmount');
+let tip = document.getElementById('tipPercent');
+let billPerPerson = document.getElementById('totalBillPerPerson');
 
-let timesIncreased = 0;
+let numberOfPersons = 0;
 increament.onclick = () => {
-    timesIncreased = timesIncreased + 1;
-    numberOfPers.innerText = timesIncreased;
-}
-
+    numberOfPersons = numberOfPersons + 1;
+    numberOfPers.innerText = numberOfPersons;
+    calculate();
+};
 
 decreament.onclick = () => {
-    if(timesIncreased <= 1 ){
+    if (numberOfPersons <= 1) {
+        alert(`Hmm... Number of persons cannot be less than one!`)
         return
     }
-        timesIncreased = timesIncreased - 1;
-        numberOfPers.innerText = timesIncreased
-    
+    numberOfPersons = numberOfPersons - 1;
+    numberOfPers.innerText = numberOfPersons
+    calculate();
+};
+const calculate = () => {
+    const bill = Number(billAmount.value);
+    const tipPercent = Number(tip.value) / 100;
+    const totalTip = Number(bill * tipPercent);
+    const totalBill = Number(totalTip + bill);
+    const persons = Number(numberOfPers.innerText);
+    const perPerson = Number(totalBill / persons);
+    billPerPerson.innerText = perPerson.toFixed(2);
 }
